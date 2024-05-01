32153de63d5a4cd88220584609605a4758942803
Alena <luned.v@yandex.ru>
2024-05-01 15:34:23 +0600

    Update lr9.cpp

--- lr9.cpp
+++ lr9.cpp
@@ -1,54 +1,93 @@
-using System;
+#include <iostream>
+#include <cstring>
+#include <vector>
 
-class MainClass
+using namespace std;
+
+struct edge
 {
-static void Main()
+int x, y;
+};
+
+int a[55][55], du[55];
+
+vector<edge> my;
+
+void jie(int u)
 {
-int scenario = 1;
-while (true)
+edge t;
+for (int v = 1; v <= 50; v++)
 {
-string[] line = Console.ReadLine().Split();
-int n = int.Parse(line[0]);
-int r = int.Parse(line[1]);
-if (n == 0 && r == 0)
+if (a[u][v])
 {
+a[u][v]--; a[v][u]--;
+t.x = u; t.y = v;
+my.push_back(t);
+jie(v);
+}
+}
+}
+
+int main()
+{
+int t, i, j, n, k = 1, u, v;
+cin » t;
+
+while (t--)
+{
+cin » n;
+memset(a, 0, sizeof(a));
+memset(du, 0, sizeof(du));
+
+for (i = 1; i <= n; i++)
+{
+cin » u » v;
+a[u][v]++;
+a[v][u]++;
+du[u]++;
+du[v]++;
+}
+
+int flag = 1;
+
+for (i = 1; i <= 50; i++)
+{
+if (du[i] % 2)
+{
+flag = 0;
+}
 break;
 }
 
-// Считываем граф и его свойства
-int[,] graph = new int[n, n];
-for (int i = 0; i < r; i++)
+if (flag)
 {
-line = Console.ReadLine().Split();
-int c1 = int.Parse(line[0]) - 1;
-int c2 = int.Parse(line[1]) - 1;
-int p = int.Parse(line[2]);
-graph[c1, c2] = p;
-graph[c2, c1] = p;
-}
-line = Console.ReadLine().Split();
-int start = int.Parse(line[0]) - 1;
-int end = int.Parse(line[1]) - 1;
-int tourists = int.Parse(line[2]);
-
-// Используем алгоритм Флойда-Уоршелла для поиска кратчайших путей
-for (int k = 0; k < n; k++)
+my.clear();
+jie(u);
+if (my.size() != n || my[0].x != my[n - 1].y)
 {
-for (int i = 0; i < n; i++)
-{
-for (int j = 0; j < n; j++)
-{
-graph[i, j] = Math.Max(graph[i, j], Math.Min(graph[i, k], graph[k, j]));
-}
+flag = 0;
 }
 }
 
-// Вычисляем минимальное число поездок
-int trips = (int)Math.Ceiling((double)tourists / (double)(graph[start, end] - 1));
+if (k != 1)
+{
+cout « endl;
+}
 
-// Выводим результат
-Console.WriteLine("Scenario #{0}", scenario++);
-Console.WriteLine("Minimum Number of Trips = {0}\n", trips);
+cout « "Case #" « k « endl;
+k++;
+
+if (!flag)
+{
+cout « "IMPOSSIBLEEE" « endl;
+}
+else
+{
+for (i = 0; i < my.size(); i++)
+{
+cout « my[i].x « " " « my[i].y « endl;
 }
 }
+}
+return 0;
 }
